2974c68db454adeef6ca3653849677bcec605d6d
piotoor <piotoor@gmail.com>
2020-07-05 22:55:19 +0200

    Implemented find_pair_with_given_sum functions

--- arrays.py
+++ arrays.py
@@ -1,10 +1,34 @@
 def find_pair_with_given_sum_n2(a, s):
-    pass
+    for i in range(len(a)):
+        for j in range(i + 1, len(a)):
+            if a[i] + a[j] == s:
+                return i, j
+    return -1, -1
 
 
 def find_pair_with_given_sum_n_logn(a, s):
-    pass
+    b = sorted(a)
+    i = 0
+    j = len(b) - 1
+
+    while i < j:
+        if b[i] + b[j] == s:
+            return i, j
+        elif b[i] + b[j] < s:
+            i += 1
+        elif b[i] + b[j] > s:
+            j -= 1
+
+    return -1, -1
 
 
 def find_pair_with_given_sum_n(a, s):
-    pass
+    h = dict()
+
+    for i in range(len(a)):
+        if s - a[i] in h:
+            return h[s - a[i]], i
+        else:
+            h[a[i]] = i
+
+    return -1, -1
